bc2548a9e283bca8feaf0275dc3a88d530ab8b20
Jin Kwon <onacit@gmail.com>
2014-09-15 16:15:48 +0900

    initial commit

---
+++ src/main/java/com/github/jinahya/test/HelloWorldXlet.java
@@ -0,0 +1,36 @@
+
+
+package com.github.jinahya.test;
+
+
+import javax.tv.xlet.Xlet;
+import javax.tv.xlet.XletContext;
+import javax.tv.xlet.XletStateChangeException;
+
+
+public class HelloWorldXlet implements Xlet {
+
+
+    public void initXlet(final XletContext xc) throws XletStateChangeException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    public void startXlet() throws XletStateChangeException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    public void pauseXlet() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    public void destroyXlet(boolean unconditional)
+        throws XletStateChangeException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+}
+
