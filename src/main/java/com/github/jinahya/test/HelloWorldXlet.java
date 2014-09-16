317c74c32a84869d4161d86ecb0a0a15331bbe3b
Jin Kwon <onacit@gmail.com>
2014-09-16 12:33:51 +0900

    microlog added

--- src/main/java/com/github/jinahya/test/HelloWorldXlet.java
+++ src/main/java/com/github/jinahya/test/HelloWorldXlet.java
@@ -3,32 +3,80 @@
 package com.github.jinahya.test;
 
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
 import javax.tv.xlet.Xlet;
 import javax.tv.xlet.XletContext;
 import javax.tv.xlet.XletStateChangeException;
+import net.sf.microlog.core.Logger;
+import net.sf.microlog.core.LoggerFactory;
+import net.sf.microlog.core.PropertyConfigurator;
 
 
 public class HelloWorldXlet implements Xlet {
 
 
+    private static final Logger logger
+        = LoggerFactory.getLogger(HelloWorldXlet.class);
+
+
+    private static final String NAME_ENVIRONMENT_PROPERTIES
+        = "/environment.properties";
+
+
     public void initXlet(final XletContext xc) throws XletStateChangeException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        PropertyConfigurator.configure("/microlog.properties");
+
+        final Properties properties = new Properties();
+        {
+            final InputStream resource = getClass().getResourceAsStream(
+                NAME_ENVIRONMENT_PROPERTIES);
+            if (resource == null) {
+                throw new XletStateChangeException(
+                    "resource not found: " + NAME_ENVIRONMENT_PROPERTIES);
+            }
+            try {
+                try {
+                    properties.load(resource);
+                } finally {
+                    resource.close();
+                }
+            } catch (final IOException ioe) {
+                logger.fatal("failed to loading " + NAME_ENVIRONMENT_PROPERTIES,
+                             ioe);
+                throw new XletStateChangeException(ioe.getMessage());
+            }
+        }
+        for (final Enumeration keys = properties.keys();
+             keys.hasMoreElements();) {
+            final String key = (String) keys.nextElement();
+            final String value = properties.getProperty(key);
+            logger.info("property: " + key + " / " + value);
+        }
     }
 
 
     public void startXlet() throws XletStateChangeException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        logger.info("startXlet()");
     }
 
 
     public void pauseXlet() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        logger.info("pauseXlet()");
     }
 
 
     public void destroyXlet(boolean unconditional)
         throws XletStateChangeException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        logger.info("destroyXlet(" + unconditional + ")");
+
+        LoggerFactory.shutdown();
     }
 
 
